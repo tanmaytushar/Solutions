a86ca506c15bb7e166c4c6f3ff148abc595918fe
tanmaytushar <ttushar176@gmail.com>
2025-06-30 15:57:08 +0530

    1483Leetcode – 1483Leetcode

--- 1483Leetcode.cpp
+++ 1483Leetcode.cpp
@@ -1,28 +1,111 @@
-/*
-Store the 2 ^ j ancestor for every node
-now Kth parent can be found out since every number can be broken down into powers of 2
-*/
-class TreeAncestor {
-public:
-    vector<vector<int>> up;
-    int LOG;
-    TreeAncestor(int n, vector<int>& parent) {
-        LOG = 20;
-        up.assign(n,vector<int>(LOG,-1));
-        for(int i = 0;i<n;i++) up[i][0] = parent[i];
-        for(int j=1;j<LOG;j++){
-            for(int i = 0;i<n;i++){
-                if(up[i][j-1] != -1) up[i][j] = up[up[i][j-1]][j-1];
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MOD 1000000007
+#define endl '\n'
+#define ll long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    int ans = 0;
+    vector<int> val(n);
+    for (int len = 1; len * 2 <= n; len++) {
+        debug(len*2); fill(val.begin(), val.end(), 0);
+        for (int i = 0; i + len < n; i++) {
+            val[i] = (s[i] == s[i + len] || s[i] == '?' || s[i + len] == '?');
+        }
+        for (int i = 1; i < n; i++) {
+            val[i] += val[i - 1];
+        }
+        for (int i = len; i < n; i++) {
+            if (val[i] - val[i - len] == len) {
+                ans = max(ans, 2 * len);
             }
         }
+        debug(val);
     }
-    int getKthAncestor(int node, int k) {
-        for(int i = 0;i<LOG;i++){
-            if(k&(1<<i)){
-                node = up[node][i];
-                if(node == -1) return -1;
-            }
-        }
-        return node;
-    }
-};
+    cout << ans << "\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+signed main() {
+   auto begin = std::chrono::high_resolution_clock::now();
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  freopen("Error.txt", "w", stderr);
+#endif
+   int t;
+   cin >> t;
+   while (t--) {
+     solve(); 
+   }
+   auto end = std::chrono::high_resolution_clock::now();
+   auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+   cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+   return 0;
+}
