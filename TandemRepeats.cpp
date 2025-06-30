bfe3273be9b40ed5cec49ad5c63acf64ab3392a3
tanmaytushar <ttushar176@gmail.com>
2025-06-30 16:03:28 +0530

    TandemRepeats – TandemRepeats

---
+++ TandemRepeats.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MOD 1000000007
+#define endl '\n'
+#define ll long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+the idea here is if you fix the Tandem len the is it decided which character should match to which character
+*/
+
+
+
+void solve() {
+  string s;cin>>s;
+  int n = s.size(),ans = 0;
+  vector<int> val(n);
+  for(int len=1; len*2<=n; len++){
+    fill(val.begin(), val.end(), 0);
+    for(int i=0; i+len<n; i++)
+      val[i] = (s[i]==s[i+len] || s[i]=='?' || s[i+len]=='?');
+    for(int i=1;i<n;i++)
+      val[i] += val[i-1];
+    if(val[len-1]==len)     ans = max(ans, 2*len);
+    for(int i=len; i<n; i++){
+      if(val[i]-val[i-len]==len)
+        ans = max(ans, 2*len);
+    }
+  }
+  cout<<ans<<"\n";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+signed main() {
+  auto begin = std::chrono::high_resolution_clock::now();
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  freopen("Error.txt", "w", stderr);
+#endif
+  int t;
+  cin >> t;
+  while (t--) {
+    solve(); 
+  }
+  auto end = std::chrono::high_resolution_clock::now();
+  auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+  cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+  return 0;
+}
