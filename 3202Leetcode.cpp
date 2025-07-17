c65633c98bb5d4c679f1fa132189ee4e03a6bd7c
tanmaytushar <ttushar176@gmail.com>
2025-07-17 09:55:55 +0530

    3202Leetcode – 3202Leetcode

--- 3202Leetcode.cpp
+++ 3202Leetcode.cpp
@@ -1,19 +1,108 @@
-/*
-Like LIS -> dp[i][k] -> max len of subsequence that ends that index i and the the (sub[t] + sub[t+1])%k is same for all t in the subsequence;
-                */
-class Solution {
-public:
-    int maximumLength(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(k,0));
-        int ans = 2;
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<i;j++){
-                int rem = (nums[i] + nums[j]) % k;
-                dp[i][rem] = (dp[j][rem] > 0 ? dp[j][rem] + 1 : 2);
-                ans = max(dp[i][rem],ans);
-            }
-        }
-        return ans;
-    }
-};
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MOD 1000000007
+#define endl '\n'
+#define ll long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void solve() {
+    int n, l, k;
+    cin >> n >> l >> k;
+    vector<int> pos(n), limit(n);
+    for (auto &it : pos) cin >> it;
+    for (auto &it : limit) cin >> it;
+    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(k + 1, vector<int>(n, -1)));
+    function<int(int, int, int)> rec = [&](int index, int left, int prev) -> int {
+        if (left < 0) return 1e9; 
+        if (index == n) return (l - pos[prev]) * limit[prev];
+        if (dp[index][left][prev] != -1) return dp[index][left][prev];
+    
+        int keep = (pos[index] - pos[prev]) * limit[prev] + rec(index + 1, left, index);
+
+        int remove = rec(index + 1, left - 1, prev);
+
+        return dp[index][left][prev] = min(keep, remove);
+    };
+    int ans = rec(1, k, 0); 
+    cout << ans << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+signed main() {
+   auto begin = std::chrono::high_resolution_clock::now();
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  freopen("Error.txt", "w", stderr);
+#endif
+   int t = 1;
+   while (t--) {
+     solve(); 
+   }
+   auto end = std::chrono::high_resolution_clock::now();
+   auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+   cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+   return 0;
+}
