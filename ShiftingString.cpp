c0d2ced404a95b2c0e7e12fff1436f60b1792861
tanmaytushar <ttushar176@gmail.com>
2025-07-17 09:25:49 +0530

    ShiftingString – ShiftingString

---
+++ ShiftingString.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define MOD 1000000007
+#define endl '\n'
+#define ll long long
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+typedef unsigned long long ull;
+typedef long double lld;
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int shift(const string& s) {
+    int len = s.size();
+    for (int i = 1; i <= len; ++i) {
+        string rotated = s.substr(i) + s.substr(0, i);
+        if (rotated == s)
+            return i;
+    }
+    return len;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> p[i];
+        p[i]--; 
+    }
+    vector<bool> used(n, false);
+    long long ans = 1;
+    for (int i = 0; i < n; ++i) {
+        if (!used[i]) {
+            string ss = "";
+            int j = i;
+            while (!used[j]) {
+                ss += s[j];
+                used[j] = true;
+                j = p[j];
+            }
+            int ln = shift(ss);
+            ans = ans * ln / gcd(ans, ln);
+        }
+    }
+    cout << ans << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+signed main() {
+   auto begin = std::chrono::high_resolution_clock::now();
+   ios::sync_with_stdio(0);
+   cin.tie(0);
+   cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("input.txt","r",stdin);
+  freopen("output.txt","w",stdout);
+  freopen("Error.txt", "w", stderr);
+#endif
+   int t;
+   cin >> t;
+   while (t--) {
+     solve(); 
+   }
+   auto end = std::chrono::high_resolution_clock::now();
+   auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+   cerr << "Time measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+   return 0;
+}
